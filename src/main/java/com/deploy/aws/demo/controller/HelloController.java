23954e301675b2665d698219a9fe3823c7980bf7
Ly Tran <congly.work@gmail.com>
2025-04-11 22:00:14 +0700

    Modify version 0.0.3

--- src/main/java/com/deploy/aws/demo/controller/HelloController.java
+++ src/main/java/com/deploy/aws/demo/controller/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
     @GetMapping
     public ResponseEntity<?> greetings() {
 
-        return new ResponseEntity<Object>("<h1 style='color:green;'>Congratulations, your app has been deployed! 04-11-2025 Ly Testing<h1>", HttpStatus.OK);
+        return new ResponseEntity<Object>("<h1 style='color:green;'>Congratulations, your app has been deployed! *04-11-2025 Ly Testing<h1>", HttpStatus.OK);
     }
 
 }
